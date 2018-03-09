0cf7e5824ced1fe4e0bf13ad9dabb48f4d85cdcc
Wendi Yu <s439178@ocsb.local>
2018-03-09 13:50:27 -0500

    Initial commit

---
+++ area_and_circumference_of_circle.py
@@ -0,0 +1,15 @@
+#
+#Created by: Wendi Yu
+#Created on: March 1 2018
+#
+#Calculate the area and circumference of circle
+#
+
+lengthofbasea = float(input("calculate the area of trapezoid: Enter length of base a = "))
+lengthofbaseb = float(input("Enter length of base b = "))
+lengthofheighth = float(input("Enter length of height h = "))
+
+areaoftrapezoid = (lengthofbasea + lengthofbaseb)* lengthofheighth/2
+print("The area is:   " + str(areaoftrapezoid))
+
+input()
